88e1c44985d05b8b821be93edc532a634267937a
FAISAL <92253635+FAISALT1000@users.noreply.github.com>
2022-08-27 23:01:40 +0300

    Add files via upload

---
+++ final one/models/order.js
@@ -0,0 +1,36 @@
+// import moment from 'moment';
+
+// class Order {
+//   constructor(id, items, totalAmount, date) {
+//     this.id = id;
+//     this.items = items;
+//     this.totalAmount = totalAmount;
+//     this.date = date;
+//   }
+
+//   get readableDate() {
+   
+//     return moment(this.date).format('MMMM Do YYYY, hh:mm');
+//   }
+// }
+
+// export default Order;\
+
+import moment from 'moment';
+
+class Order {
+  constructor(id, items, totalAmount, date) {
+    this.id = id;
+    this.items = items;
+    this.totalAmount = totalAmount;
+    this.date = date;
+  }
+
+  get readableDate() {
+   
+    return moment(this.date).format('MMMM Do YYYY, hh:mm');
+  }
+}
+
+export default Order;
+
